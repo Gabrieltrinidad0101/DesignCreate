324822df978e5394dabffcd13152fc2611d8cc22
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-03-21 22:32:36 -0400

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+function App (): JSX.Element {
+  return (
+    <div className="App">
+      <div>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+export default App
