8b3d8aae725a3b237f697ad02a7d4a7a0d3e69c5
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-03-25 20:53:31 -0400

    feat: share domian

---
+++ frontend/src/share/domian/prop.ts
@@ -0,0 +1,3 @@
+export default interface Prop<T> {
+  Prop: T
+}
---
+++ frontend/src/share/infranstruture/toast.ts
@@ -0,0 +1,9 @@
+import type IToast from '../domian/IToast'
+import { toast } from 'react-toastify'
+
+const Toast: IToast = {
+  sucess: (texto: string) => { toast.success(texto) },
+  error: (texto: string) => { toast.error(texto) }
+}
+
+export default Toast
