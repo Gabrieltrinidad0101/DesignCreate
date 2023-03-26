869fa9de524a033909e151b55c2d2aa58b82ab2a
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-03-25 20:59:47 -0400

    feat: share infranstruture

---
+++ frontend/src/share/application/imagesContainer.ts
@@ -0,0 +1,7 @@
+import '../../types/imagesContainer.d'
+import logo from '../../assets/images/logo.png'
+import type IImagesContainer from '../domian/IImageContainer'
+const imagesContainer: IImagesContainer = {
+  logo
+}
+export default imagesContainer
