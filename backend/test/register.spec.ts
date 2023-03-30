9b7351ba35c22a2d8c5121234ed4120935552bf6
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-03-29 22:09:36 -0400

    feat: eslint-fix

--- backend/test/register.spec.ts
+++ backend/test/register.spec.ts
@@ -1,27 +1,27 @@
-import app from "../src/app";
-import request from "supertest";
+import app from '../src/app'
+import request from 'supertest'
 
-describe("POST /register", () => {
-    test("register with error", async () => {
-        const response = await request(app).post("/register").send({
-            password: "12345"
-        })
-        expect(response.statusCode).toBe(500);
+describe('POST /register', () => {
+  test('register with error', async () => {
+    const response = await request(app).post('/register').send({
+      password: '12345'
     })
+    expect(response.statusCode).toBe(500)
+  })
 
-    test("register", async () => {
-        const response = await request(app).post("/register").send({
-            name: "juan",
-            password: "12345"
-        })
-        expect(response.statusCode).toBe(200);
+  test('register', async () => {
+    const response = await request(app).post('/register').send({
+      name: 'juan',
+      password: '12345'
     })
+    expect(response.statusCode).toBe(200)
+  })
 
-    test("register user exist", async () => {
-        const response = await request(app).post("/register").send({
-            name: "juan",
-            password: "12345"
-        })
-        expect(response.statusCode).toBe(409);
+  test('register user exist', async () => {
+    const response = await request(app).post('/register').send({
+      name: 'juan',
+      password: '12345'
     })
+    expect(response.statusCode).toBe(409)
+  })
 })
