fab13a02fe5378d5c0ffe0296eb46aac429970d6
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-04-02 19:21:47 -0400

    feat: findByNameAndPassword in userRepository

--- backend/src/mooc/authentication/domain/IUserRepository.ts
+++ backend/src/mooc/authentication/domain/IUserRepository.ts
@@ -1,5 +1,6 @@
 import type IUser from './IAuthentication'
 export default interface IUserRepository {
-  insert: (user: IUser) => Promise<void>
+  insert: (user: IUser) => Promise<IUser>
   findByName: (name: string) => Promise<IUser | null>
+  findByNameAndPassword: (name: string, password: string) => Promise<IUser | null>
 }
--- backend/src/mooc/authentication/infranstructure/Database/userRepository.ts
+++ backend/src/mooc/authentication/infranstructure/Database/userRepository.ts
@@ -4,10 +4,17 @@ import { UserModel } from './userSchema'
 import { ErrorInsertUser } from '../../domain/IErrorUser'
 
 export default class UserRepository implements IUserRepository {
-  async insert (user: IUser): Promise<void> {
+  async insert (user: IUser): Promise<IUser> {
     try {
       const newUser = new UserModel(user)
       await newUser.save()
+      const userSave: IUser = {
+        name: newUser.name,
+        password: newUser.password,
+        isRegister: newUser.isRegister,
+        _id: newUser._id as string
+      }
+      return userSave
     } catch {
       throw new ErrorInsertUser()
     }
@@ -16,6 +23,15 @@ export default class UserRepository implements IUserRepository {
   async findByName (name: string): Promise<IUser | null> {
     try {
       const user: IUser | null = await UserModel.findOne({ name })
+      return user
+    } catch {
+      throw new ErrorInsertUser()
+    }
+  }
+
+  async findByNameAndPassword (name: string, password: string): Promise<IUser | null> {
+    try {
+      const user: IUser | null = await UserModel.findOne({ name, password })
       return user
     } catch {
       throw new ErrorInsertUser()
