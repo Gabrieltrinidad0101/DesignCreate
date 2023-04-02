790d6a379544f8f9cdacee5ebe9d9472a5fbaf2e
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-04-02 19:22:36 -0400

    feat: cors and morgan

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express'
 import { authRouter } from './mooc/authentication/infranstructure/authRouter'
+import cors from 'cors'
+import morgan from 'morgan'
 import './database'
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
 app.use('/', authRouter)
 export default app
