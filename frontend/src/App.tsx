d844c898a68e5fa9857127acc2b11aa031325c8c
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-04-03 23:13:54 -0400

    feat: login and home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import Authentication from './screens/authentication/infranstruture/Authentication'
-
+import Authentication from './screens/authentication/infranstructure/Authentication'
+import Home from './screens/home/insfranstructure/Home'
+import './App.css'
 export default function App (): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Authentication/>}></Route>
+        <Route path="/login" element={<Authentication/>}></Route>
+        <Route path="/home" element={<Home/>}></Route>
       </Routes>
       <ToastContainer/>
     </BrowserRouter>
