0aa05f9c30236004471f03d538ac34411dd7d372
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-04-07 14:10:21 -0400

    fix: database connection

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,4 +1,4 @@
 import mongosee from 'mongoose'
-mongosee.connect('mongodb://localhost/designcreate')
+mongosee.connect('mongodb://mongo/designcreate')
   .then(res => { console.log('Db is connected') })
   .catch(error => { console.error(error) })
