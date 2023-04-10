6f1141629c8f0b23abfc9e3456e9d590847034ec
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-04-09 20:16:13 -0400

    feat: authentication provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,18 @@ import { ToastContainer } from 'react-toastify'
 import Authentication from './screens/authentication/infranstructure/Authentication'
 import Home from './screens/home/insfranstructure/Home'
 import './App.css'
+import { AuthenticationProvider } from './share/infranstruture/AuthenticationContext'
 export default function App (): JSX.Element {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Authentication/>}></Route>
-        <Route path="/login" element={<Authentication/>}></Route>
-        <Route path="/home" element={<Home/>}></Route>
-      </Routes>
-      <ToastContainer/>
-    </BrowserRouter>
+          <Routes>
+            <Route path="/register" element={<Authentication isRegister={true} />}></Route>
+            <Route path="/login" element={<Authentication isRegister={false} />}></Route>
+            <Route element={<AuthenticationProvider />} >
+                <Route path="/home" element={<Home />}></Route>
+            </Route>
+          </Routes>
+          <ToastContainer />
+        </BrowserRouter>
   )
 }
---
+++ frontend/src/share/infranstruture/AuthenticationContext.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState, useContext } from 'react'
+import { customFecth } from './dependencies'
+import type IHttpResult from '../../../../share/domain/httpResult'
+import { type IUser } from '../../../../share/domain/user'
+import type IUserState from '../domian/user'
+import { Outlet, useNavigate } from 'react-router-dom'
+
+const userInitialState = {
+  name: '',
+  password: '',
+  isRegister: false
+}
+
+const AuthContext = React.createContext<IUserState>({
+  user: userInitialState,
+  setUser: (user: IUser) => user
+})
+
+const AuthenticationProvider = (): JSX.Element => {
+  const [user, setUser] = useState<IUser>(userInitialState)
+  const navigation = useNavigate()
+
+  const verifyAuthentication = async (): Promise<void> => {
+    try {
+      const result = await customFecth.get<IHttpResult>('/verifyAuthentication')
+      setUser(result?.message)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    verifyAuthentication()
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  const containerSetUser = (user: IUser): void => {
+    setUser(prevUser => ({ ...prevUser, ...user }))
+  }
+
+  if (user === undefined) {
+    navigation('/register')
+  }
+
+  return <AuthContext.Provider value={{ user, setUser: containerSetUser }} >{<Outlet/>}</AuthContext.Provider>
+}
+export const useAuthenticationContext = (): IUserState => {
+  return useContext<IUserState>(AuthContext)
+}
+
+export { AuthContext, AuthenticationProvider }
