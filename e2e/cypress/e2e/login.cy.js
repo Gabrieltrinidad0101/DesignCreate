693512cbdc8875b73beebdcddaf4820d381bbd9d
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-04-16 11:30:29 -0400

    refactor:  url

--- e2e/cypress/e2e/login.cy.js
+++ e2e/cypress/e2e/login.cy.js
@@ -1,8 +1,9 @@
-describe('Authentication', () => {
-  beforeEach(()=>{
-    cy.visit('http://localhost:5173/register')
-
+const url = 'http://localhost:3000';
+describe('Authentication register', () => {
+  beforeEach(() => {
+    cy.visit(`${url}/register`)
   })
+
   it('register without name', () => {
     cy.get("input[name='password']").type("juan")
     cy.get("#auth-button").click();
@@ -17,16 +18,51 @@ describe('Authentication', () => {
 
   it('register', () => {
     register("Welcome juan")
+
+    cy.getAllLocalStorage().then((result) => {
+      console.log(result)
+      expect(result[url]["token"]).to.exist
+    })
   })
-  it("register user exists",()=>{
+
+  it("register user exists", () => {
     register("The user exists")
   })
 
 })
 
+describe('Authentication login', () => {
+  beforeEach(() => {
+    cy.visit(`${url}/login`)
+  })
 
-const register = (spectedText)=>{
-  cy.get("input[name='name']").type("juan")
+  it("login user no exists", () => {
+    register("The username or password is incorrect","peppe")
+  })
+
+  it('login', () => {
+    register("Welcome juan")
+    cy.getAllLocalStorage().then((result) => {
+      expect(result[url]["token"]).to.exist
+    })
+  })
+})
+
+
+describe('Authentication', () => {
+  beforeEach(() => {
+    cy.visit(`${url}/home`)
+  })
+
+  it('Verify Redirect', async () => {
+    cy.wait(1000);
+    cy.contains("Error try to login");
+    cy.url().should('include', '/register')
+  })
+})
+
+const register = (spectedText, name = "juan") => {
+  cy.get("input[name='name']").type(name)
   cy.get("input[name='password']").type("juan")
   cy.get("#auth-button").click();
   cy.contains(spectedText);
