f18618d52bec90f533cf7cc7cfb9a39e2f1254c5
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-04-23 13:32:25 -0400

    feat: editor header

---
+++ frontend/src/screens/editor/infranstructure/components/header/Header.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import HeaderCss from './Header.module.css'
+import type IProp from '../../../../../share/domian/prop'
+import { type IEditorEvents } from '../../../domain/IEditor'
+export default function Header ({ Prop: editorEvents }: IProp<IEditorEvents>): JSX.Element {
+  const changeEvent = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    editorEvents.changeName(e.target.value)
+  }
+
+  const save = (): void => {
+    editorEvents.save()
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  return (
+        <div className={HeaderCss.container}>
+            <div className={HeaderCss.logoContainer}>
+                DesignCreate
+            </div>
+            <div className={HeaderCss.containerDesignName}>
+                <input type="text" value={editorEvents.designName} onChange={changeEvent} placeholder='Design name' />
+            </div>
+            <div className={`${HeaderCss.ctrls} ${HeaderCss.icon}`}>
+                <div>
+                    <i className="fa-solid fa-arrow-rotate-right"></i>
+                </div>
+                <div>
+                    <i className="fa-solid fa-arrow-rotate-left"></i>
+                </div>
+            </div>
+            <div className={`${HeaderCss.buttoSaveAndClose} ${HeaderCss.icon}`}>
+                <div onClick={save}>
+                    <i className="fa-solid fa-floppy-disk"></i>
+                </div>
+                <div>
+                    <i className="fa-solid fa-download"></i>
+                </div>
+                <div>
+                    <i className="fa-solid fa-xmark"></i>
+                </div>
+            </div>
+        </div>
+  )
+}
