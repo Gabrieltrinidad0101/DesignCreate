662e361541998566c3f96ba2838aab26fa740abd
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-04-23 13:31:19 -0400

    feat: dynamic type in IHttpResult

--- share/domain/httpResult.ts
+++ share/domain/httpResult.ts
@@ -1,7 +1,9 @@
-export default interface IHttpResult {
-    message: any
+import IDesign from "./design"
+
+export default interface IHttpResult<T>{
+    message: T
 }
 
-export interface IHttpStatusCode extends IHttpResult{
+export interface IHttpStatusCode extends IHttpResult<any>{
     statusCode?: number
 }
