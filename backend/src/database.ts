6ce4158499a5039a36170c837691d3503936b86e
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-05-14 20:41:30 -0400

    refactor: mongodb conection and share volumes

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -2,7 +2,6 @@ import mongosee from 'mongoose'
 
 let connection = 'mongodb://'
 if (process.env.DBUSER !== undefined && process.env.DBPASSWORD !== undefined) connection = `mongodb://${process.env.DBUSER}:${process.env.DBPASSWORD}@`
-console.log(connection)
-mongosee.connect(`${connection}${process.env.HOST ?? 'localhost'}/designcreate`)
+mongosee.connect(`${connection}localhost:27018/designcreate`)
   .then(res => { console.log('Db is connected') })
   .catch(error => { console.error(error) })
