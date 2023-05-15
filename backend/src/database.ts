a6ad9e1b01b5bb79f4e4ae8bcf687c286e9b0dfc
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-05-14 20:51:53 -0400

    fix: port

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -2,6 +2,7 @@ import mongosee from 'mongoose'
 
 let connection = 'mongodb://'
 if (process.env.DBUSER !== undefined && process.env.DBPASSWORD !== undefined) connection += `${process.env.DBUSER}:${process.env.DBPASSWORD}@`
-mongosee.connect(`${connection}${process.env.HOST ?? "localhost"}:27018/designcreate`)
+console.log(`${connection}${process.env.HOST ?? "localhost"}/designcreate`)
+mongosee.connect(`${connection}${process.env.HOST ?? "localhost"}/designcreate`)
   .then(res => { console.log('Db is connected') })
   .catch(error => { console.error(error) })
