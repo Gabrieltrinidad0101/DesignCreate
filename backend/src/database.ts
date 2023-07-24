f3745a826524f8a2a332856d5444e994d3cdb693
Gabrieltrinidad0101 <73808842+Gabrieltrinidad0101@users.noreply.github.com>
2023-07-24 12:50:01 -0400

    Change port of db

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,6 +1,6 @@
 import mongosee from 'mongoose'
 
 let connection = 'mongodb://'
-mongosee.connect(`${connection}${process.env.HOST ?? "localhost"}:27017/designcreate`)
+mongosee.connect(`${connection}${process.env.HOST ?? "localhost"}:27018/designcreate`)
   .then(res => { console.log('Db is connected') })
   .catch(error => { console.error(error) })
