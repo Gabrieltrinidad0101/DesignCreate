a236f1970f4f2de845c68ed503757a2077e988c5
gabriel <gabrielqwes123@gmail.com>
2024-06-14 11:58:32 -0400

    fix: docker

--- docker-pro.sh
+++ docker-pro.sh
@@ -1,2 +1,2 @@
-docker compose -f ./backend/docker-compose.yml up backend_pro mongo_pro -d --build
+docker compose -f ./backend/docker-compose.yml up mongo_pro -d --build
 docker compose -f ./nginx/docker-compose.yml -d --build
