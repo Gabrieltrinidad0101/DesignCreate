dc1feee9316d8d375c238559914179493c08f9be
gabriel <gabrielqwes123@gmail.com>
2024-06-23 15:13:25 -0400

    fix: service name

--- docker-pro.sh
+++ docker-pro.sh
@@ -1,2 +1,2 @@
 docker network create development
-docker compose -f ./backend/docker-compose.yml up --build -d mongo_pro designcreate_backend_pro
+docker compose -f ./backend/docker-compose.yml up --build -d mongo_pro backend_pro
